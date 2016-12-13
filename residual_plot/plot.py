2a2c49eb3c10bc270c606cd8b417e2640b587ddf
brandoncarr09 <brandoncarr09@gmail.com>
2016-12-13 15:06:24 -0600

    ok

--- residual_plot/plot.py
+++ residual_plot/plot.py
@@ -87,6 +87,18 @@ def plot_Scatter(fig, x, y, subx1, suby2, subsize, title, xlabel, ylabel,linesty
     ax.set_xticklabels(ax.get_xticks(), size=8)
     ax.set_yticklabels(ax.get_yticks(), size=8)
     return ax
+    
+def plot_fill(fig, x, y, subx1, suby2, subsize, title, xlabel, ylabel,linestyle, errlow=[], errhigh=[]):
+    ax = fig.add_axes([subx1, suby2, subsize, subsize])
+    ax.plot(x, y,'k', linestyle = linestyle)
+    ax.fill_between(x, errlow, errhigh, facecolor='k', alpha=0.5)
+    ax.set_title(title, fontsize=8)
+    ax.set_xlabel(xlabel, fontsize=8)
+    ax.set_ylabel(ylabel, fontsize=8)
+    ax.set_xlim(max(x),min(x))
+    ax.set_xticklabels(ax.get_xticks(), size=8)
+    ax.set_yticklabels(ax.get_yticks(), size=8)
+    return ax
 
 cmdfile = open(cmdfile_name, 'r')
 line = cmdfile.readline()
@@ -99,7 +111,7 @@ res_arr, sig_arr = np.zeros(shape), np.zeros(shape)
 for i in range(shape[0]):
     for j in range(shape[1]):
         line = cmdfile.readline().split()
-        mag, col, obs, mod, res, sig = [float(n) for n in line[:6]]
+        mag, col, obs, mod, res, sig = [float(n) for n in line[:5]]
         colbins[j] = col
         obs_arr[i, j], mod_arr[i, j] = obs, mod
         res_arr[i, j], sig_arr[i, j] = res, sig
@@ -117,11 +129,14 @@ Tf_arr = np.zeros(71)
 met_arr = np.zeros(71)
 met_up = np.zeros(71)
 met_low = np.zeros(71)
+csf_arr = np.zeros(71)
+csf_up = np.zeros(71)
+csf_low = np.zeros(71)
 line =outfile.readline()
 totalSF = float(line.split()[1])
 for i in range(71):
     line = outfile.readline().split()
-    age, Tf, x, sfr, y, z, met, metup, metdown = [float(n) for n in line[:9]]
+    age, Tf, x, sfr, y, z, met, metup, metdown, a, b, c, csf, csfup, csfdown = [float(n) for n in line[:15]]
     lage_arr[i]=age
     sfr_arr[i]=sfr
     sfr_high[i] = y
@@ -129,6 +144,9 @@ for i in range(71):
     met_arr[i]=met
     met_up[i] = metup
     met_low[i] = metdown
+    csf_arr[i] = csf
+    csf_up[i] = csfup
+    csf_low[i] = csfdown
 outfile.close
 
 age_arr = [10**(n)/1e9 for n in lage_arr]
@@ -160,9 +178,9 @@ ax_2 = plot_Scatter(fig, lage_arr, sfr_arr, subx2, suby2, subsize, '(e) Star For
 
 ax5 = plot_Scatter(fig, age_arr, sfr_arr, subx3, suby2, subsize, '(f) Star Formation Rate', 'age (Gyr)','SFR','steps', sfr_low, sfr_high)
 
-ax_1 = plot_Scatter(fig, lage_arr, csfr_arr, subx2, suby3, subsize, '(b) Cumulative Star Formation', 'log(age)','SF','solid')
+ax_1 = plot_fill(fig, lage_arr, csf_arr, subx2, suby3, subsize, '(b) Cumulative Star Formation', 'log(age)','SF','solid', csf_low, csf_up)
 
-ax_4 = plot_Scatter(fig, age_arr, csfr_arr, subx3, suby3, subsize, '(c) Cumulative Star Formation', 'age (Gyr)', 'SF','solid')
+ax_4 = plot_fill(fig, age_arr, csf_arr, subx3, suby3, subsize, '(c) Cumulative Star Formation', 'age (Gyr)', 'SF','solid', csf_low, csf_up)
 
 
 ax_3 = plot_Scatter(fig, lage_arr, met_arr, subx2, suby1, subsize, '(h) Metallicity', 'log(age)','Z','solid', met_low, met_up)
