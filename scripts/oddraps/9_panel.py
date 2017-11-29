35198c119fb500faac3cbb8c9018c3f6ee72f771
Melissa Morris <melmor@login1.wrangler.tacc.utexas.edu>
2017-11-29 10:58:59 -0600

    made 9_panel.py useable on tacc

--- scripts/oddraps/9_panel.py
+++ scripts/oddraps/9_panel.py
@@ -9,7 +9,7 @@
 import numpy as np
 import matplotlib
 #uncomment line below for TACC usage
-#matplotlib.use("Agg")
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.colors as cl
 import sys
